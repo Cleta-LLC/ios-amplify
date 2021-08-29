aa3998702c213bd86ba52a3d04e6d15080d7a38f
Sergio Sierra <sergiosierra@cleta.io>
2021-08-29 18:27:32 -0400

    Initial Commit

---
+++ ios-amplify/ios_amplifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_amplifyApp.swift
+//  ios-amplify
+//
+//  Created by John Eliwood on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_amplifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
